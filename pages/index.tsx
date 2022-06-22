5454a2bf3acb6b23205fb1143f935bb10a88c7e8
franco-pozzi <franco663@hotmail.com>
2022-06-21 21:51:37 -0300

    Base boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+
+import Head from "next/head";
+import Image from "next/image";
+
+import { Layout } from "../components/layouts";
+
+import styles from "../styles/Home.module.css";
+
+import { Typography } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
-  <></>
-    );
-}
+    <Layout title="Home Page | OpenJira">
+      <Typography variant="h1" color="primary">
+        Open Jira - APP
+      </Typography>
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
