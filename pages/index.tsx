2386a0276e9f2eb288e84a0937aaedb57d4b224a
franco-pozzi <franco663@hotmail.com>
2022-07-01 16:54:54 -0300

    Add NewEntry component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import { Layout } from "../components/layouts";
 
 import { Card, CardContent, CardHeader, Grid } from "@mui/material";
-import { EntryList } from "../components/ui";
+import { EntryList, NewEntry } from "../components/ui";
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title='Pendientes' />
             <CardContent>
+              <NewEntry />
               <EntryList status="pending" />
             </CardContent>
           </Card>
