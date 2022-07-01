56e5f583b427e0c5830a1e5a407af4849561f94c
franco-pozzi <franco663@hotmail.com>
2022-07-01 15:31:31 -0300

    Add entries context to app

--- context/entries/EntriesContext.tsx
+++ context/entries/EntriesContext.tsx
@@ -1,7 +1,8 @@
 import { createContext } from 'react';
+import { Entry } from '../../interfaces';
 
 export interface ContextProps {
-    entries: [];
+    entries: Entry[];
 }
 
 export const EntriesContext = createContext({} as ContextProps);
--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,15 +1,37 @@
 import { FC, useReducer } from 'react';
 import { EntriesContext, entriesReducer } from './';
 
+import { Entry } from '../../interfaces';
+
+import { v4 as uuidv4 } from 'uuid';
 type EntriesProviderProps = {
     children: React.ReactNode;
 };
 export interface EntriesState {
-    entries: [];
+    entries: Entry[];
 }
 
 export const Entries_INITIAL_STATE: EntriesState = {
-    entries: [],
+    entries: [
+        {
+            _id: uuidv4(),
+            description: 'A modo de test se muestra informacion',
+            status: 'pending',
+            createdAt: Date.now(),
+        },
+        {
+            _id: uuidv4(),
+            description: 'A modo de test se muestra informacion',
+            status: 'finished',
+            createdAt: Date.now() - 1400000,
+        },
+        {
+            _id: uuidv4(),
+            description: 'A modo de test se muestra informacion',
+            status: 'in-progress',
+            createdAt: Date.now() - 100000,
+        },
+    ],
 };
 
 export const EntriesProvider: FC<EntriesProviderProps> = ({ children }) => {
