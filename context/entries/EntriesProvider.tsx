e1d4be55954fbd1dd0cab38d5d61ac3a10b0eec6
franco-pozzi <franco663@hotmail.com>
2022-07-23 21:40:00 -0300

    Connect update entry in frontend

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -46,8 +46,14 @@ export const EntriesProvider: FC<EntriesProviderProps> = ({ children }) => {
         }
     }
 
-    const updateEntry = (entry: Entry) => {
-        dispatch({ type: '[Entry] Entry-Updated', payload: entry })
+    const updateEntry = async ({ _id, description, status }: Entry) => {
+        try {
+            const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, { description, status })
+
+            dispatch({ type: '[Entry] Entry-Updated', payload: data })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
