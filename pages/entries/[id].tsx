ed8606d35b48421cb050e1a92423f99c41f4ed0f
franco-pozzi <franco663@hotmail.com>
2022-09-16 17:21:29 -0300

    Add entry details page

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { ChangeEvent, useState, useMemo } from 'react';
+
 import { Layout } from '../../components/layouts';
-import { Card, Grid } from '@mui/material';
+
+import { capitalize, Card, Grid, CardHeader, CardContent, TextField, CardActions, Button, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, IconButton } from '@mui/material';
+
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+
+import { EntryStatus } from '../../interfaces';
+
+const validStatus: EntryStatus[] = ['pending', 'in-progress', 'finished'];
 
 const EntryPage = () => {
+    const [inputValue, setInputValue] = useState('')
+    const [status, setStatus] = useState<EntryStatus>('pending')
+    const [touched, setTouched] = useState(false)
+
+    const onFieldChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value)
+    }
+
+    const onStatusChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setStatus(event.target.value as EntryStatus)
+    }
+
+    const onSave = () => {
+        console.log({ inputValue, status })
+    }
+
+    const isNotValid = useMemo(() => !inputValue.length && touched, [inputValue, touched])
+
     return (
         <Layout title='....'>
             <Grid
@@ -11,10 +38,60 @@ const EntryPage = () => {
                 sx={{ marginTop: 2 }} >
                 <Grid item xs={12} sm={8} md={6}>
                     <Card>
-                        <CardHeader title='' />
+                        <CardHeader title={`Entrada: ${inputValue}`} subheader={`Creada hace ... minutos`} />
+
+                        <CardContent>
+                            <TextField
+                                sx={{ marginTop: 2, marginBottom: 1 }}
+                                fullWidth
+                                placeholder='Nueva entrada'
+                                autoFocus
+                                multiline
+                                label='Nueva entrada'
+                                value={inputValue}
+                                onChange={onFieldChanged}
+                                onBlur={() => setTouched(true)}
+                                helperText={isNotValid && 'Ingrese un valor'}
+                                error={isNotValid}
+                            />
+
+                            <FormControl>
+                                <FormLabel>Estado:</FormLabel>
+                                <RadioGroup row value={status} onChange={onStatusChange}>
+                                    {
+                                        validStatus.map(option => (
+                                            <FormControlLabel
+                                                key={option}
+                                                value={option}
+                                                control={<Radio />}
+                                                label={capitalize(option)}
+                                            />
+                                        ))
+                                    }
+                                </RadioGroup>
+                            </FormControl>
+                        </CardContent>
+
+                        <CardActions>
+                            <Button
+                                startIcon={<SaveOutlinedIcon />}
+                                variant='contained'
+                                fullWidth
+                                onClick={onSave}
+                                disabled={!inputValue.length}
+                            >
+                                SAVE
+                            </Button>
+                        </CardActions>
                     </Card>
                 </Grid>
             </Grid>
+
+            <IconButton
+                sx={{ position: 'fixed', bottom: 30, right: 30, backgroundColor: 'error.dark' }}
+            >
+                <DeleteForeverOutlinedIcon />
+            </IconButton>
 
         </Layout>
     )
